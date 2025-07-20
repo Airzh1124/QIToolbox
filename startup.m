deab88c42998686e391157ee8e6eb5efc4539014
“Airzh1124” <u3609849@connect.hku.hk>
2025-07-21 01:10:33 +0800

    Add startup.m for setting up path

---
+++ startup.m
@@ -0,0 +1,4 @@
+% QIToolbox/startup.m
+  addpath(genpath('src')); % Includes L0bit/, utils/, inequalities/, etc.
+  addpath(genpath('examples')); % Includes example scripts
+  disp('QIToolbox paths loaded.');
