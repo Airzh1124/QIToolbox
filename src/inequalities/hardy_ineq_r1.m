7548fe08774f3fff6a83a22034a834c5e6253fd5
“Airzh1124” <u3609849@connect.hku.hk>
2025-07-21 11:42:56 +0800

    Add hardy_r1 inequality and its example

---
+++ src/inequalities/hardy_ineq_r1.m
@@ -0,0 +1,18 @@
+function [hardy_r1_coefficient_matrix,dims] = hardy_ineq_r1()
+    dims.mA = 2; dims.mB = 2; dims.oA = 2; dims.oB = 2; n=1;
+    I = zeros(dims.oA^n, dims.oB^n, dims.mA^n, dims.mB^n);
+    penalty_coeff = -1e10;
+
+
+    %Set constraints
+    I(1,1,1,1) = penalty_coeff;
+    I(1,2,2,1) = penalty_coeff;
+    I(2,1,1,2) = penalty_coeff;
+
+    %Set the inequality
+    I(1,1,2,2) = 1;
+
+
+    hardy_r1_coefficient_matrix = I;
+
+end
